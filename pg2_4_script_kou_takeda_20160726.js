1b54ad5f5b5386912e93b1658ead989fd1e436f8
竹田滉 <kou.takeda@std.code.ac.jp>
2016-11-21 22:06:19 +0900

    自己紹介追加

---
+++ pg2_4_script_kou_takeda_20160726.js
@@ -0,0 +1,16 @@
+﻿function sw(){
+   if(document.getElementById("swich").value == "切り替え")
+   {
+	document.getElementById("swich").value = "戻す";
+	document.getElementById("tx").innerHTML = "<font color=\"blue\"><td>フリガナ";
+	document.getElementById("tx2").innerHTML = "タケダコウ";
+	alert("画面を切り替えました。");
+   }
+   else
+   {
+	document.getElementById("swich").value = "切り替え";
+	document.getElementById("tx").innerHTML = "<font color=\"red\">名前";
+	document.getElementById("tx2").innerHTML = "竹田滉";
+	alert("画面を戻しました。");
+   }
+}
